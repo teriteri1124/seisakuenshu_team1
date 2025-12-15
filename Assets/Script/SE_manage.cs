8c533a9c49819f56f15fc675fcf419fceaf525b4
teriteri <f5gg2t2zk@i.softbank.jp>
2025-12-16 00:44:49 +0900

    洞窟のＢＧＭの実装

---
+++ Assets/Script/SE_manage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private AudioSource audioSource = null;
+    public AudioClip SE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GameOver();
+        }
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        if (audioSource != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.Log("audiosource=null");
+        }
+    }
+
+    public void GameOver()
+    {
+        PlaySE(SE);
+    }
+}
